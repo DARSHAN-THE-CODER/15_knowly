37e215d84da1da845ba92edecbd46fadaf175556
phaneesh <phaneesh707@gmail.com>
2023-04-06 19:53:01 +0530

    editor related changes

---
+++ frontend/components/editor/components/Question.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import { quest } from '../constants/quest';
+
+const Question = () => {
+
+    const [data,setData] = useState();
+    // setData(quest[0]);
+
+    // useEffect(async ()=>{
+    //    axios.get('').then((res)=>{
+
+    //    }).catch((error)=>{
+    //     console.log("Failed to fetch question",error);
+    //    })
+        
+
+    // },[])
+
+  return (
+    <div className='flex'>
+      <div className="w-full p-4 shadow-md lg:max-w-lg">
+            <div className="space-y-2">
+                <h3 className="text-2xl font-semibold">
+                    Question
+                </h3>
+                <p className="text-gray-600">
+                    question.....
+                </p>
+            </div>
+      </div>
+           
+        
+
+    </div>
+  )
+}
+
+export default Question
