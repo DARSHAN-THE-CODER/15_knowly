91d55e5f564a014b89f6a11a6f897dcd1bb306ba
DARSHAN-THE-CODER <reachdarshanv@gmail.com>
2023-04-09 01:48:32 +0530

    ss

--- frontend/constants/api.js
+++ frontend/constants/api.js
@@ -2,6 +2,6 @@ export const APIURL = 'https://knowly.live/api/v1';
 
 export const MODEL = "https://knowly.live/api/model"
 
-export const ENDPOINT = "https://34.235.154.254:8001"
+export const ENDPOINT = "http://34.235.154.254:8001"
 
 // const APIURL = 'http://localhost:3000/api/v1';
